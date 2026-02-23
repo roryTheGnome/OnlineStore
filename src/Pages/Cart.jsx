46e4b6b4282c5492342d7e7d8e3a7fe36e2820b3
roryTheGnome <s29991@pjwstk.edu.pl>
2026-02-23 11:25:27 +0100

    STYLE - Cart.css added

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {useSelector, useDispatch} from "react-redux";
 import {removeFromCart,increaseQua,decreaseQua,clearCart} from "../cartSlice";
+import "./Cart.css"
 
 function Cart(){
     const {items}=useSelector(state=>state.cart);
@@ -9,28 +10,30 @@ function Cart(){
     const total=items.reduce((sum,item)=>sum+(item.price*item.quantity),0);
 
     return(
-        <div>
-            <h2>Cart</h2>
+        <section className="cart">
+            <h2 className="cart-title">Cart</h2>
 
             {items.map(item=>(
-                <div key={item.id}>
-                    <h4>{item.title}</h4>
-                    <p>{item.price} pln</p>
-                    <p>Quantity: {item.quantity}</p>
+                <div className="cart-products" key={item.id}>
+                    <h4 className="cart-product-title">{item.title}</h4>
+                    <p className="cart-product-price">{item.price} pln</p>
+                    <p className="cart-product-qua">Quantity: {item.quantity}</p>
 
-                    <button onClick={()=>dispatch(increaseQua(item.id))}>+</button>
-                    <button onClick={()=>dispatch(decreaseQua(item.id))}>-</button>
-                    <button onClick={()=>dispatch(removeFromCart(item.id))}>Remove</button>
+                    <div className="cart-buttons">
+                        <button className="cart-product-button" onClick={()=>dispatch(increaseQua(item.id))}>+</button>
+                        <button className="cart-product-button" onClick={()=>dispatch(decreaseQua(item.id))}>-</button>
+                        <button className="cart-product-button" onClick={()=>dispatch(removeFromCart(item.id))}>Remove</button>
+                    </div>
 
                 </div>
             ))}
 
-            <h3>Total: ${total.toFixed(2)}</h3>
+            <h3 className="cart-sum">Total: ${total.toFixed(2)}</h3>
 
-            <button onClick={()=>dispatch(clearCart())}>Empty Cart</button>
-            <button onClick={()=>alert("You are being directed to payment system")}>Pay</button>
+            <button className="cart-button" onClick={()=>dispatch(clearCart())}>Empty Cart</button>
+            <button className="cart-button" onClick={()=>alert("You are being directed to payment system")}>Pay</button>
 
-        </div>
+        </section>
     )
 }
 export default Cart
